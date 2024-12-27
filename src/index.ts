b03e90b572d643117f09270c8cd6b26e16eda4ff
Daniel Henrique <henriqueevaldo@outlook.com>
2024-12-26 22:36:47 -0300

    feat: add health-check route
    Co-authored-by: JulioC090 <JulioC090@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -99,6 +99,11 @@ app.delete('/client', (req: Request, res: Response) => {
     return res.status(200).json({message: "Client deleted successfully."})
 });
 
+// Health check
+app.get('/health-check', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
