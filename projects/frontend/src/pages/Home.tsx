0ea47925e6bb437d346348197bca75b205b59040
Daniel Henrique <henriqueevaldo@outlook.com>
2024-12-29 16:48:56 -0300

    feat: add frontend template
    Co-authored-by: JulioC090 <JulioC090@users.noreply.github.com>

---
+++ projects/frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Container } from '../components/Container';
+
+export const Home = () => {
+    return (
+        <Container variant="breakpointPadded">
+            <h1 className="scroll-m-20 text-lg font-extrabold tracking-tight lg:text-5xl justify-center text-center">
+                Steam Hour Bot
+            </h1>
+        </Container>
+    );
+}
