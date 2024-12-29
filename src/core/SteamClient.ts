f355e0de2f5a5abbd7f5150130420f1cb177244b
Daniel Henrique <henriqueevaldo@outlook.com>
2024-12-29 14:57:48 -0300

    refactor: move default replies to properly configuration file
    Co-authored-by: JulioC090 <JulioC090@users.noreply.github.com>

--- src/core/SteamClient.ts
+++ src/core/SteamClient.ts
@@ -3,6 +3,7 @@ import { log } from "../utils";
 import { getScriptUptime, getTimeInGMT3, shuffleArray } from "../utils";
 import { SteamClientInfo } from "../models/SteamClientInfo";
 import { EPersonaState } from "../models/EPersonaState";
+import defaultReplies from "../config/defaultReplies.json";
 
 export default class SteamClient {
     private steamUser: SteamUser;
@@ -25,24 +26,7 @@ export default class SteamClient {
 
         // @ts-ignore due to wrong typing in library
         this.steamUser.on('friendMessage', (steamID, message) => {
-            const conversationReplies: { [key: string]: string } = {
-                "oi": "Eai, blz?",
-                "jogar": "Bora, só deixa eu terminar essa.",
-                "sim": "Pode crê",
-                "não": "De boa, a gente troca ideia outra hora!",
-                "tchau": "Vlw man, flw",
-                "obrigado": "Dnd",
-                "como você está": "Tudo certo por aqui! E contigo?",
-                "vamos jogar agora": "Estou um pouco ocupado agora, mas depois podemos jogar!",
-                "qual jogo você gosta": "Gosto de vários jogos, ultimamente tenho jogado alguns clássicos. E você?",
-                "que horas são": "Boa pergunta! Dá uma olhada aí no relógio do dispositivo.",
-                "qual é seu nome": "Ah, apenas alguém pronto pra jogar. E o seu?",
-                "está aí?": "Estou por aqui, sim. O que precisa?",
-                "preciso de ajuda": "Claro, só dizer o que precisa e vamos resolver!",
-                "vamos marcar um dia": "Boa ideia! Só me dizer quando seria bom para você.",
-                "quais jogos você tem": "Ah, alguns bem legais, depende do que gosta. Quer sugerir algo?",
-                "qual seu nível?": "Eu diria que sou mediano! Depende do jogo também, e você?"
-            };
+            const conversationReplies: { [key: string]: string } = defaultReplies;
 
             // Define interaction replies
             const interactionReplies: { [key: string]: () => string } = {
